100d35ec28caa7d162ce505081e88625124dde61
Nicolai Czempin <nicolai.czempin@gmail.com>
2017-03-31 00:41:19 +0200

    Add github url

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(name='gym_random_walk',
       version='0.0.1',
-      install_requires=['gym']  
+      install_requires=['gym'],
+      url="https://github.com/nczempin/gym-random-walk"
 )  
 
