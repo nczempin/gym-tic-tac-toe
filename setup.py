e19382cc2b1f422de0e54f3802312d3278aba26d
Nicolai Czempin <nicolai.czempin@gmail.com>
2017-03-31 00:48:43 +0200

    This is necessary for any actual installation taking place

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='gym_random_walk',
       version='0.0.1',
       install_requires=['gym'],
-      url="https://github.com/nczempin/gym-random-walk"
+      url="https://github.com/nczempin/gym-random-walk",
+      packages=find_packages()
 )  
-
